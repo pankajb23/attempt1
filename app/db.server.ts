8d87dfab446c8223fbd2e74dd7ecce565b8ae190
pankaj bhardwaj <pankaj.bhardwaj.2100@gmail.com>
2025-01-15 15:12:04 +0530

    adding debug log

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg';
-const { Pool } = pg;
+
 const connectionString = `${process.env.DATABASE_URL}`
-let adapter = new PrismaPg(new Pool({ connectionString }))
+
+console.log(" connection String -> ",connectionString)
+const pool = new pg.Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 declare global {
   var prisma: PrismaClient;
