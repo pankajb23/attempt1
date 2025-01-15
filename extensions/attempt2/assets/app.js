f4dcb6cc8d730fdf64e00485eb443930d6ca9790
pankaj bhardwaj <pankaj.bhardwaj.2100@gmail.com>
2025-01-15 17:12:34 +0530

    changes

--- extensions/attempt2/assets/app.js
+++ extensions/attempt2/assets/app.js
@@ -39,7 +39,8 @@ const TotalPriceCrossedOutTextColor = "component.total.price.crossed.out.text.co
 
 function getHost() {
     // return "https://3dcf-2401-4900-1f37-cbfd-1d35-c7a8-c9fb-a870.ngrok-free.app";
-    return "https://7a81-2401-4900-1f37-2772-2951-b107-9e03-f06d.ngrok-free.app";
+    // return "https://7a81-2401-4900-1f37-2772-2951-b107-9e03-f06d.ngrok-free.app";
+    return "https://sellcross-bc95eb582641.herokuapp.com/";
     // return `${location.origin}/apps/store`;
 }
 
